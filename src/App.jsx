2b281c9f52e4800bdff1efa05d5332a0c94a656d
nickonyi <nickoluoch1130@gmail.com>
2026-02-06 11:41:12 +0300

    feat: implement dashboard layout with sidebar and placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './styles/style.css';
 
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <>
-      <Login />
+      <Dashboard />
     </>
   );
 }
